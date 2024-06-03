374f99c036710aeca394a263c9ec35fd9e994807
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-06-03 15:32:58 +0300

    01

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div style={{ backgroundImage: `url(${bg})`}} className='bg-red-600 grid grid-cols-2 '>
+      <div style={{ backgroundImage: `url(${bg})`}} className='bg-red-600 grid grid-cols-2 items-center'>
         <div className='flex justify-center items-center flex-wrap content-center gap-4'>
           <h1 className='text-4xl color-white text-white font-bold'>Learn to code by watching others</h1>
           <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
